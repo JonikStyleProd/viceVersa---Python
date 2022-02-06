4ed00026e517b4318364a06aeb0c6bcdf20353e7
JonikStyle <tydiet1@gmail.com>
2022-02-06 12:33:25 +0200

    Full reverse text app!

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('reversed/', views.reverse, name="reverse"),
 
 ]
